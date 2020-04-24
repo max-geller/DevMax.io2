87c3aaab11b1273b9f7396d6a011623eaa739462
Max Geller <mxgeller@gmail.com>
2020-04-24 12:41:42 -0700

    added stats page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import { FeedComponent } from './components/feed/feed.component';
 import { BrowseTagsComponent } from './components/browse-tags/browse-tags.component';
 import { BrowseCoursesComponent } from './components/browse-courses/browse-courses.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
+import { StatsComponent } from './components/stats/stats.component';
 
 @NgModule({
       declarations: [
         FeedComponent,
         BrowseTagsComponent,
         BrowseCoursesComponent,
-        ReviewsComponent
+        ReviewsComponent,
+        StatsComponent
     ],
     imports: [
         CommonModule,
