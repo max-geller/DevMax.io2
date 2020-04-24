faaeb90c8f49e766b44fb50767d5138a722806ac
Max Geller <mxgeller@gmail.com>
2020-04-23 19:04:06 -0700

    adding particles js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var particlesJS: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  myStyle: object = {};
-  myParams: object = {};
-  width: number = 100;
-  height: number = 100;
+export class AppComponent implements OnInit {
 
-  ngOnInit() {
-      this.myStyle = {
-          'position': 'fixed',
-          'width': '100%',
-          'height': '100%',
-          'z-index': -1,
-          'top': 0,
-          'left': 0,
-          'right': 0,
-          'bottom': 0,
-      };
-
-  this.myParams = {
-          particles: {
-              number: {
-                  value: 200,
-              },
-              color: {
-                  value: '#ff0000'
-              },
-              shape: {
-                  type: 'triangle',
-              },
-      }
-  };
+    title="particle";
+  
+    constructor(){}
+    ngOnInit(){
+      
+      particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+    }
+  
   }
-}
