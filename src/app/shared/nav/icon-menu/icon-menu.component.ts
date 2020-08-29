90ed4910240f4b376ba4c2f39517e30ff2198998
Max Geller <mxgeller@gmail.com>
2020-08-29 16:52:01 -0700

    configuring features

--- src/app/shared/nav/icon-menu/icon-menu.component.ts
+++ src/app/shared/nav/icon-menu/icon-menu.component.ts
@@ -1,14 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-icon-menu',
   templateUrl: './icon-menu.component.html',
 })
-export class IconMenuComponent implements OnInit {
+export class IconMenuComponent {
+  events: string[] = [];
+  opened: boolean;
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(public auth: AuthService, private afs: AngularFirestore) { }
+  titles: any[];
+  isShowSearch = false;
 
-  ngOnInit(): void {
+  get isAuthenticated(): boolean {
+    return this.auth.isAuthenticated;
   }
 
+  toggleSearch() {
+    this.isShowSearch = !this.isShowSearch;
+  }
+
+  signOut() {
+    this.auth.signOut();
+  }
+
+  openSupport() {
+    window.open("https://support.devmax.io", "_blank");
+  }
+
+  openLearning() {
+    window.open("https://learning.devmax.io", "_blank");
+  }
+
+  openDocumentation() {
+    window.open("https://docs.devmax.io", "_blank");
+  }
+
+  openSlack() {
+    window.open("https://devmax.slack.com", "_blank");
+  }
 }
