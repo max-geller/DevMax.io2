a7aa11451f5e468e7d69fbfa9568e91e0999b0c5
Max Geller <mxgeller@gmail.com>
2020-08-30 14:48:02 -0700

    updating page layouts

--- src/app/shared/nav/sidenav/sidenav.component.ts
+++ src/app/shared/nav/sidenav/sidenav.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
 
