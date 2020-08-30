aa79c6cb6ecc8d6b1636a1cf5628c336d25affe1
Max Geller <mxgeller@gmail.com>
2020-08-30 10:31:20 -0700

    tie to course database

--- src/app/features/courses/pages/course-detail/course-detail.component.ts
+++ src/app/features/courses/pages/course-detail/course-detail.component.ts
@@ -1,14 +1,35 @@
+import { CourseInterface } from './../../../../core/models/course.model';
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FirestoreService } from 'src/app/core/services/firestore.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
 })
 export class CourseDetailComponent implements OnInit {
+  public course: CourseInterface = {
+    id: null
+  };
 
-  constructor() { }
+  constructor(private dataApi: FirestoreService, public afs: AngularFirestore, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    const idCourse = this.route.snapshot.params['id'];
+    this.getDetails(idCourse);
+    console.log(idCourse);
+
+  }
+
+  getDetails(idCourse: string): void {
+    this.dataApi.getOneCourse(idCourse).subscribe(course => {
+      this.course = course;
+
+    });
   }
 
 }
