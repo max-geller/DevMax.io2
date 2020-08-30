cba990cf2d2fb30fbf8c303c2195695d34d7794a
Max Geller <mxgeller@gmail.com>
2020-08-29 21:00:32 -0700

    courses development

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -6,12 +6,30 @@ import { CourseDetailComponent } from './pages/course-detail/course-detail.compo
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreModule } from 'src/app/core/core.module';
 import { CoursesComponent } from './courses.component';
+import { CourseFilterComponent } from './components/course-filter/course-filter.component';
+import { CourseSummaryComponent } from './components/course-summary/course-summary.component';
+import { CourseContentComponent } from './components/course-content/course-content.component';
+import { FeaturedReviewComponent } from './components/featured-review/featured-review.component';
+import { AlsoBoughtComponent } from './components/also-bought/also-bought.component';
+import { InstructorBioComponent } from './components/instructor-bio/instructor-bio.component';
+import { StudentFeedbackComponent } from './components/student-feedback/student-feedback.component';
+import { CourseReviewsComponent } from './components/course-reviews/course-reviews.component';
+import { CourseSidebarComponent } from './components/course-sidebar/course-sidebar.component';
 
 
 @NgModule({
   declarations: [
     CoursesComponent,
-    CourseDetailComponent
+    CourseDetailComponent,
+    CourseFilterComponent,
+    CourseSummaryComponent,
+    CourseContentComponent,
+    FeaturedReviewComponent,
+    AlsoBoughtComponent,
+    InstructorBioComponent,
+    StudentFeedbackComponent,
+    CourseReviewsComponent,
+    CourseSidebarComponent
   ],
   imports: [
     CommonModule,
