6801fd4f0cb089793013587907db7ef052b47c05
Max Geller <mxgeller@gmail.com>
2020-08-30 14:21:21 -0700

    updating pages

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
 
--- src/app/pages/tags/tags.component.ts
+++ src/app/pages/tags/tags.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.scss']
 })
 export class TagsComponent implements OnInit {
 
