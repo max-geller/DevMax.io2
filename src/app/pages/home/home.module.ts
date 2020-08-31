1ec31a8b2f729409640fc0e507dbfcbca3243c0a
Max Geller <mxgeller@gmail.com>
2020-08-31 11:21:11 -0700

    removed banner

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -10,7 +10,6 @@ import { ReviewsComponent } from './components/reviews/reviews.component';
 import { StatsComponent } from './components/stats/stats.component';
 import { CtaComponent } from './components/cta/cta.component';
 import { HomeComponent } from './home.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @NgModule({
@@ -22,7 +21,6 @@ import { NewsletterComponent } from './components/newsletter/newsletter.componen
         ReviewsComponent,
         StatsComponent,
         CtaComponent,
-        BannerComponent,
         NewsletterComponent,
     ],
     imports: [
